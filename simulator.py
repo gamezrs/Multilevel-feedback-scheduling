8f4dd64a44faad542be43482768634cb72e76918
BingChilling873 <psr.amir@gmail.com>
2024-06-28 23:14:47 +0200

    Finish basic queue processing

--- simulator.py
+++ simulator.py
@@ -51,28 +51,28 @@ def process_queues(tasks: list):
     while not is_every_queue_empty():
         TIME += 1
 
-        for task in tasks:
-            name = task[0]
-            runtime = task[1]
-            arrival = task[2]
+        for task in tasks: # Loop through tasks
+            if task[2] == TIME: # Check if the current task arrives now
+                QUEUES[0].append(task) # Add the new task to the end of the queue with the highest priority
 
-            # Check if a new process arrived
-            if arrival == TIME:
-                QUEUES[0].append(task)
+        for queue_id, queue in enumerate(QUEUES): # Loop through all queues
+            if len(queue) > 0: # Check for the first not empty queue
+                quantum = QUANTUM[queue_id] # Get the quantum of the queue
+                task = queue[0] # Get the first task in the queue
 
-        for queue_id, queue in enumerate(QUEUES):
-            quantum = QUANTUM[queue_id]
-
-            if len(queue) > 0:
-                task = queue[0]
-
-                if not task[3]:
-                    task[3] = 0
+                if not task[3]: # Check if the task has run in the current quantum
+                    task[3] = 0 # Set the used time of the current quantum to zero
 
                 task[1] -= 1 # decrease the remaining needed CPU time
                 task[3] += 1 # increase the time used in the current quantum
 
+                if task[1] == 0: # Check if the task has finished
+                    queue.pop(0) # Remove the task from the queue
+
+                if task[3] == quantum: # Check if the task has used up his time in the current quantum
+                    move_task_to_end_of_queue(queue) # Move task to the end of the current queue
                 
+                break # Abort the Loop and start from the beginning, a time unit has passed
 
 
 
@@ -82,6 +82,12 @@ def is_every_queue_empty():
             return False
     
     return True
+
+
+def move_task_to_end_of_queue(queue: list):
+    task = queue.pop(0)
+    del task[3]
+    queue.append(task)
 
 
 def get_next_scheduled_task():
